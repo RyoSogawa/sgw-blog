cf67e421294bcc7f4ad0349ef250942b73cd3e28
RyoSogawa <koashimitekara.1122@gmail.com>
2022-03-21 22:50:14 +0900

    add tailwind plugins and setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,55 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx,css,mdx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx,css}', './_posts/**/*.md'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    fontFamily: {},
+    zIndex: {
+      hide: -1,
+      auto: 'auto',
+      base: 0,
+      docked: 10,
+      dropdown: 1000,
+      sticky: 1100,
+      banner: 1200,
+      overlay: 1300,
+      modal: 1400,
+      popover: 1500,
+      skipLink: 1600,
+      toast: 1700,
+      tooltip: 1800,
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          md: '2rem',
+        },
+      },
+      colors: {},
+    },
   },
-  plugins: [],
+  variants: {
+    typography: ['dark'],
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-font-size'),
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1048px',
+          },
+        },
+      })
+    },
+  ],
 }
