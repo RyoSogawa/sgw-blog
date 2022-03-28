66fba446a6fab7b28a991bab0de1ee2f87a78b33
RyoSogawa <koashimitekara.1122@gmail.com>
2022-03-28 12:13:49 +0900

    cleanup constants.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,34 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
+// import Post from '../types/post'
 
-type Props = {
-  allPosts: Post[]
-}
-
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+// type Props = {
+//   allPosts: Post[]
+// }
+//{ allPosts }: Props
+const Index = () => {
+  // const heroPost = allPosts[0]
+  // const morePosts = allPosts.slice(1)
   return (
     <>
-      <Layout>
+      <div>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Next.js Blog Example with</title>
         </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+        <div>
+          {/*{heroPost && (*/}
+          {/*  <HeroPost*/}
+          {/*    title={heroPost.title}*/}
+          {/*    coverImage={heroPost.coverImage}*/}
+          {/*    date={heroPost.date}*/}
+          {/*    author={heroPost.author}*/}
+          {/*    slug={heroPost.slug}*/}
+          {/*    excerpt={heroPost.excerpt}*/}
+          {/*  />*/}
+          {/*)}*/}
+          {/*{morePosts.length > 0 && <MoreStories posts={morePosts} />}*/}
+        </div>
+      </div>
     </>
   )
 }
