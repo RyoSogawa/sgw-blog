1aa7174acc385bedbd8ce839556378c8e6d7a600
RyoSogawa <koashimitekara.1122@gmail.com>
2022-03-29 23:50:03 +0900

    add Layout component

---
+++ src/components/common/Layout/Layout.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Layout from './Layout'
+
+export default {
+  title: 'common/Layout',
+  component: Layout,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Layout>
+
+const Template: ComponentStory<typeof Layout> = ({ children, ...rest }) => (
+  <Layout {...rest}>{children}</Layout>
+)
+
+export const DefaultStyle = Template.bind({})
+DefaultStyle.args = {
+  children: 'content',
+}
---
+++ src/components/common/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+
+export type LayoutProps = {
+  children: React.ReactNode
+}
+
+const Layout: React.VFC<LayoutProps> = ({ children }) => {
+  return (
+    <div className={'flex flex-col min-h-screen'}>
+      <Header />
+      <div className={'grow'}>{children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
