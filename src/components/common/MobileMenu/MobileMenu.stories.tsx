a43f2d0f232553b64cdb7ac3a9a48d0ee3ee0f9b
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-01 18:06:03 +0900

    show MobileMenu in its story

--- src/components/common/MobileMenu/MobileMenu.stories.tsx
+++ src/components/common/MobileMenu/MobileMenu.stories.tsx
@@ -1,14 +1,21 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import MobileMenu from './MobileMenu'
+import { useRef } from 'react'
 
 export default {
   title: 'common/MobileMenu',
   component: MobileMenu,
 } as ComponentMeta<typeof MobileMenu>
 
-const Template: ComponentStory<typeof MobileMenu> = props => (
-  <MobileMenu {...props} />
-)
+const Template: ComponentStory<typeof MobileMenu> = props => {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  return (
+    <div>
+      <button ref={buttonRef}>dummy</button>
+      <MobileMenu {...props} burgerRef={buttonRef} />
+    </div>
+  )
+}
 
 export const DefaultStyle = Template.bind({})
 DefaultStyle.args = {}
