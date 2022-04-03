06015f29a7ef2e30694d1e7d329487caf63bded6
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-03 22:19:24 +0900

    fix HeadingProps type

--- src/components/ui/Heading/Heading.tsx
+++ src/components/ui/Heading/Heading.tsx
@@ -1,7 +1,7 @@
 import React, { HTMLProps } from 'react'
 import cn from 'classnames'
 
-export type HeadingProps = HTMLProps<HTMLHeadingElement> & {
+export type HeadingProps = Omit<HTMLProps<HTMLHeadingElement>, 'size'> & {
   className?: string
   as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
