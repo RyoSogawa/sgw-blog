bf50f06d8dce85e80614c4c3da4531c27a4adc59
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-03 17:15:34 +0900

    add HTMLHeadingProps to Heading component

--- src/components/ui/Heading/Heading.tsx
+++ src/components/ui/Heading/Heading.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { HTMLProps } from 'react'
 import cn from 'classnames'
 
-export type HeadingProps = {
+export type HeadingProps = HTMLProps<HTMLHeadingElement> & {
   className?: string
   as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
@@ -13,6 +13,7 @@ const Heading: React.VFC<HeadingProps> = ({
   as,
   size = as,
   children,
+  ...rest
 }) => {
   const fontSizeClasses: { [k in HeadingProps['as']]: string } = {
     h1: 'fsz-48ptr',
@@ -23,15 +24,20 @@ const Heading: React.VFC<HeadingProps> = ({
     h6: 'fsz-24ptr',
   }
   const fontSizeClass = fontSizeClasses[size]
-  const baseClass = `font-inter font-bold text-white ${fontSizeClass}`
+  const baseClass = `text-center font-inter font-bold text-white ${fontSizeClass}`
+
+  const props: HTMLProps<HTMLHeadingElement> = {
+    className: cn(baseClass, className),
+    ...rest,
+  }
 
   const element: { [k in HeadingProps['as']]: JSX.Element } = {
-    h1: <h1 className={cn(baseClass, className)}>{children}</h1>,
-    h2: <h2 className={cn(baseClass, className)}>{children}</h2>,
-    h3: <h3 className={cn(baseClass, className)}>{children}</h3>,
-    h4: <h4 className={cn(baseClass, className)}>{children}</h4>,
-    h5: <h5 className={cn(baseClass, className)}>{children}</h5>,
-    h6: <h6 className={cn(baseClass, className)}>{children}</h6>,
+    h1: <h1 {...props}>{children}</h1>,
+    h2: <h2 {...props}>{children}</h2>,
+    h3: <h3 {...props}>{children}</h3>,
+    h4: <h4 {...props}>{children}</h4>,
+    h5: <h5 {...props}>{children}</h5>,
+    h6: <h6 {...props}>{children}</h6>,
   }
 
   return element[as]
