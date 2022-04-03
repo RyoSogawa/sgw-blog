18ea2c2311be90f1b6713290c87be2575dfbe698
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-03 21:51:22 +0900

    create index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import Head from 'next/head'
+import BgGradient from '../components/common/BgGradient'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Ryo Sogawa</title>
+      </Head>
+      <div className={'fixed top-0 left-0 z-hide w-screen h-screen'}>
+        <BgGradient
+          bgColorClassNames={['bg-lightGreen', 'bg-yellow-600', 'bg-pink-500']}
+        />
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,83 @@
+import type { NextPage } from 'next'
+import type Post from '../types/post'
 import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-// import Post from '../types/post'
+import PostCard from '../components/post/PostCard'
+import Layout from '../components/common/Layout'
+import Heading from '../components/ui/Heading'
+import Author from '../components/author/Author'
+import ButtonLink from '../components/ui/ButtonLink'
 
-// type Props = {
-//   allPosts: Post[]
-// }
-//{ allPosts }: Props
-const Index = () => {
-  // const heroPost = allPosts[0]
-  // const morePosts = allPosts.slice(1)
+type Props = {
+  posts: Post[]
+}
+
+const PageIndex: NextPage<Props> = ({ posts }) => {
   return (
-    <>
-      <div>
-        <Head>
-          <title>Next.js Blog Example with</title>
-        </Head>
-        <div>
-          {/*{heroPost && (*/}
-          {/*  <HeroPost*/}
-          {/*    title={heroPost.title}*/}
-          {/*    coverImage={heroPost.coverImage}*/}
-          {/*    date={heroPost.date}*/}
-          {/*    author={heroPost.author}*/}
-          {/*    slug={heroPost.slug}*/}
-          {/*    excerpt={heroPost.excerpt}*/}
-          {/*  />*/}
-          {/*)}*/}
-          {/*{morePosts.length > 0 && <MoreStories posts={morePosts} />}*/}
-        </div>
+    <Layout>
+      <div className={'container grid place-items-center h-screen'}>
+        <blockquote className={'font-inter font-bold text-center fsz-48ptr'}>
+          “Everything in It’s Right Place”
+          <span className="block font-extralight fsz-11ptr">
+            --Radiohead 2000
+          </span>
+        </blockquote>
       </div>
-    </>
+      <section className="section">
+        <div className="container max-w-[700px]">
+          <Heading as={'h2'} id={'about'}>
+            About
+          </Heading>
+          <div className="grid gap-6 mt-8 lg:grid-flow-col lg:gap-12">
+            <Author className={'shrink-0'} imageType={'photo'} />
+            <div>
+              <p>
+                香川県出身福岡県在住の 33 歳 1
+                児の父。フリーランスでエンジニアをしています。
+              </p>
+              <p className={'mt-4'}>
+                React を使ったWebフロントエンドを中心に、企画 / 要件定義 / UI
+                デザイン / システム設計 / バックエンド / インフラ /
+                ネイティブアプリ等もやっています。
+              </p>
+            </div>
+          </div>
+          <div className="mt-8 text-center">
+            <ButtonLink href={'/about'} theme={'secondary'}>
+              More About Me
+            </ButtonLink>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className={'container'}>
+          <Heading as={'h2'} id={'posts'}>
+            Posts
+          </Heading>
+          <div className="grid gap-6 mt-4 lg:grid-flow-col lg:mt-8">
+            {posts.map(post => (
+              <PostCard post={post} key={post.slug} />
+            ))}
+          </div>
+          <div className="mt-8 text-center lg:mt-12">
+            <ButtonLink href={'/posts'} theme={'secondary'}>
+              See All Posts
+            </ButtonLink>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <Heading as={'h2'} id={'works'}>
+            Works
+          </Heading>
+          <p className="mt-8 text-center">Now Developing...🛠</p>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
-export default Index
+export default PageIndex
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
