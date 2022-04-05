fe5efa04e8a9f404c96b86d0e0b5e288155b656a
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-05 22:30:01 +0900

    add Button component

---
+++ src/components/ui/Button/Button.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Button from './Button'
+
+export default {
+  title: 'ui/Button',
+  component: Button,
+  argTypes: { onClick: { action: 'onClick' } },
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = props => <Button {...props} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  children: 'Button Text',
+}
+
+export const PrimaryWithShadow = Template.bind({})
+PrimaryWithShadow.args = {
+  shadow: true,
+  children: 'Button Text',
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  theme: 'secondary',
+  children: 'Button Text',
+}
+
+export const SecondaryWithShadow = Template.bind({})
+SecondaryWithShadow.args = {
+  theme: 'secondary',
+  shadow: true,
+  children: 'Button Text',
+}
---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import cn from 'classnames'
+
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  theme?: 'primary' | 'secondary'
+  shadow?: boolean
+}
+
+const Button: React.VFC<ButtonProps> = ({
+  className,
+  theme = 'primary',
+  shadow,
+  children,
+  ...rest
+}) => {
+  return (
+    <button
+      {...rest}
+      className={cn(
+        className,
+        'inline-block py-2 px-6 min-w-[170px] font-inter font-medium text-center rounded border fsz-16ptr',
+        'transition-colors duration-200',
+        theme === 'primary' &&
+          'text-white bg-blue-500 hover:bg-blue-600 border-blue-500 hover:border-blue-600',
+        theme === 'secondary' &&
+          'text-black hover:text-white bg-white hover:bg-transparent border-white',
+        shadow && 'shadow',
+        shadow && theme === 'primary' && 'shadow-blue-500'
+      )}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
