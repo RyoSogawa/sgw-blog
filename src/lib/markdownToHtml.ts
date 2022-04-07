1c85a24720dba5ad76a8fe8e94d1224a16fa0db0
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-07 23:13:03 +0900

    yarn remove rehype-document

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -3,7 +3,6 @@ import remarkParse from 'remark-parse'
 import remarkToc from 'remark-toc'
 import slug from 'remark-slug'
 import remarkRehype from 'remark-rehype'
-import rehypeDocument from 'rehype-document'
 import rehypeFormat from 'rehype-format'
 import rehypeStringify from 'rehype-stringify'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -16,7 +15,6 @@ export default async function markdownToHtml(markdown: string) {
     .use(remarkToc, { heading: '目次', tight: true, maxDepth: 3 })
     .use(remarkRehype)
     .use(shiki, { theme: 'hc_black' })
-    .use(rehypeDocument, { title: 'Contents' })
     .use(rehypeFormat)
     .use(rehypeStringify)
     .process(markdown)
