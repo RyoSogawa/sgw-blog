8daeb3624ad10ebf51691a541b47389f25ba0fb0
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-07 23:24:26 +0900

    disable eslint for line to file

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkToc from 'remark-toc'
@@ -5,7 +6,6 @@ import slug from 'remark-slug'
 import remarkRehype from 'remark-rehype'
 import rehypeFormat from 'rehype-format'
 import rehypeStringify from 'rehype-stringify'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const shiki = require('rehype-shiki')
 
 export default async function markdownToHtml(markdown: string) {
