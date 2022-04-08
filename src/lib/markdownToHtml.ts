57ad2716a7fa0363ae187ad08211ffda3213e33f
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-08 21:33:06 +0900

    add auto-heading-link to md

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -4,6 +4,7 @@ import remarkParse from 'remark-parse'
 import remarkToc from 'remark-toc'
 import slug from 'remark-slug'
 import remarkRehype from 'remark-rehype'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeFormat from 'rehype-format'
 import rehypeStringify from 'rehype-stringify'
 const shiki = require('rehype-shiki')
@@ -14,6 +15,23 @@ export default async function markdownToHtml(markdown: string) {
     .use(slug)
     .use(remarkToc, { heading: '目次', tight: true, maxDepth: 3 })
     .use(remarkRehype)
+    .use(rehypeAutolinkHeadings, {
+      behavior: 'append',
+      content: {
+        type: 'element',
+        tagName: 'span',
+        properties: {
+          className:
+            'ml-2 opacity-50 font-inter transition-opacity hover:opacity-80',
+        },
+        children: [
+          {
+            type: 'text',
+            value: '#',
+          },
+        ],
+      },
+    })
     .use(shiki, { theme: 'hc_black' })
     .use(rehypeFormat)
     .use(rehypeStringify)
