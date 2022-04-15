27ecea67683028e3cd47e83a280e05f41572256e
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-15 09:28:16 +0900

    change BgGradient opacity with scroll position

--- src/components/common/BgGradient/BgGradient.tsx
+++ src/components/common/BgGradient/BgGradient.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import cn from 'classnames'
 import s from './BgGradient.module.css'
+import useScrollPosition from '../../../lib/hooks/useScrollPosition'
 
 export type BgGradientProps = {
   bgColorClassNames: string[]
 }
 
 const BgGradient: React.VFC<BgGradientProps> = ({ bgColorClassNames }) => {
+  const { y } = useScrollPosition()
+
   return (
     <section className="overflow-hidden relative w-screen h-screen">
       <div className={s.gradientWrapper} role="region">
         {bgColorClassNames.map((c, index) => (
-          <div key={c + index} className={cn(s.gradient, c)} />
+          <div
+            key={c + index}
+            className={cn(s.gradient, c)}
+            style={{ opacity: y < 300 ? 0.8 : 0.3 }}
+          />
         ))}
       </div>
     </section>
---
+++ src/lib/hooks/useScrollPosition.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+const useScrollPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition({ x: window.scrollX, y: window.scrollY })
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return scrollPosition
+}
+
+export default useScrollPosition
