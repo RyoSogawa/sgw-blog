0d6a088d3c053acdff1ca5c3539bbf9a9af72275
RyoSogawa <koashimitekara.1122@gmail.com>
2022-04-20 22:54:17 +0900

    building rss page

---
+++ src/lib/feed.ts
@@ -0,0 +1,59 @@
+/**
+ * @see https://fwywd.com/tech/next-feed-rss-atom
+ */
+import fs from 'fs'
+import { Feed } from 'feed'
+import { getAllPosts } from './api'
+import markdownToHtml from './markdownToHtml'
+import { htmlToDesc } from './utils'
+
+// TODO: link
+const generatedRssFeed = async () => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || ''
+  const date = new Date()
+  const author = {
+    name: 'Ryo Sogawa',
+    email: 'koashimitekara.1122@gmail.com',
+    link: 'https://...com',
+  }
+
+  const feed = new Feed({
+    title: process.env.NEXT_PUBLIC_BASE_NAME || '',
+    description: process.env.NEXT_PUBLIC_BASE_DISC,
+    id: baseUrl,
+    link: baseUrl,
+    language: 'ja',
+    image: `${baseUrl}/favicon.png`,
+    copyright: `All rights reserved ${date.getFullYear()}, ${author.name}`,
+    updated: date,
+    feedLinks: {
+      rss2: `${baseUrl}/rss/feed.xml`,
+      // json: `${baseUrl}/rss/feed.json`,
+      // atom: `${baseUrl}/rss/atom.xml`,
+    },
+    author,
+  })
+
+  const posts = getAllPosts(['title', 'slug', 'content', 'publishedAt'])
+
+  for (const post of posts) {
+    const url = `${baseUrl}/blog/${post.slug}`
+    const htmlContent = await markdownToHtml(post.content)
+    const desc = htmlToDesc(htmlContent)
+    feed.addItem({
+      title: post.title,
+      description: desc,
+      id: url,
+      link: url,
+      content: htmlContent,
+      date: new Date(post.publishedAt),
+    })
+  }
+
+  fs.mkdirSync('./public/rss', { recursive: true })
+  fs.writeFileSync('./public/rss/feed.xml', feed.rss2())
+  // fs.writeFileSync('./public/rss/atom.xml', feed.atom1())
+  // fs.writeFileSync('./public/rss/feed.json', feed.json1())
+}
+
+export default generatedRssFeed
